4a6d438972506f26769a9fb140ec87227d7971c8
stevgonzalezb <stevengonzalez957@gmail.com>
2021-10-19 21:53:26 -0600

    last changes after first college review

---
+++ ArquisoftApp/Controllers/ProjectController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArquisoftApp.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Project
+        public ActionResult Index()
+        {
+            return View("~/Views/Maintenance/ProjectList.cshtml");
+        }
+
+        public ActionResult Instance(int Id)
+        {
+            return View("~/Views/Maintenance/ProjectMaintenance.cshtml");
+        }
+    }
+}
