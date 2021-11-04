28f48532cdd01bf91461273f4b14ce0e5e71bd18
stevgonzalezb <stevengonzalez957@gmail.com>
2021-11-03 22:42:19 -0600

    fixing model conflicts

--- ArquisoftApp/Models/State.cs
+++ ArquisoftApp/Models/State.cs
@@ -17,6 +17,7 @@ namespace ArquisoftApp.Models
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public State()
         {
+            this.Users = new HashSet<Users>();
             this.Materials = new HashSet<Materials>();
             this.Projects = new HashSet<Projects>();
             this.Budgets = new HashSet<Budgets>();
@@ -25,6 +26,8 @@ namespace ArquisoftApp.Models
         public int idState { get; set; }
         public string description { get; set; }
     
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Users> Users { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Materials> Materials { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
